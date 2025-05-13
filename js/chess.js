c39ee3c42e2f7752d16fdb4139a75c7d9d8b55e3
Evan Thomas <thomasevan1248@gmail.com>
2025-05-13 18:24:57 -0400

    Started Implementing Turn Messages

--- js/chess.js
+++ js/chess.js
@@ -1,1 +1,11 @@
 // Later, we could implement Monte Carlo for robot logic, but that's WAY in the future
+
+let userName = "xXN00BSL4Y3RXx";
+console.log(userName);
+
+const msgDisplay = document.getElementById("turnMessages");
+const playerTurnMsg = `Your turn, ${userName}.`;
+
+msgDisplay.textContent = playerTurnMsg;
+console.log(msgDisplay)
+console.log(playerTurnMsg);
