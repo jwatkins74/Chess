67430e34f8c72bf723520aba2154b3d0c6fd8d4a
jwatkins74 <watkins.joshua321@gmail.com>
2025-05-14 14:29:12 -0400

    added little functionality to board
    made pawn able to move

--- js/chess.js
+++ js/chess.js
@@ -9,3 +9,89 @@ const playerTurnMsg = `Your turn, ${userName}.`;
 msgDisplay.textContent = playerTurnMsg;
 console.log(msgDisplay)
 console.log(playerTurnMsg);
+
+const board = document.getElementById("board")
+
+let piece;
+board.addEventListener("click", function(event) {
+    if (event.target == board) {
+        return;
+    }
+    if (piece != null){
+        if (piece.className == "a") {
+            piece.style.backgroundColor = "honeydew";
+        } else {
+            piece.style.backgroundColor = "tan";
+        }
+    }
+    if (event.target.style.backgroundColor == "green") {
+        event.target.textContent = piece.textContent
+        piece.textContent = ""
+        return;
+    }
+    piece = event.target;
+    let id = event.target.id;
+    let text = piece.textContent;
+    let new1;
+    if (text != ""){
+        piece.style.backgroundColor = "chocolate";
+        if ( text =="♙") {
+            if (id)
+            if (id[0] == 2) {
+                new1 = document.getElementById("3" + id[1])
+                if ( new1.textContent == null|| new1.textContent.length == undefined || new1.textContent.length ==0) {
+                    new1.style.backgroundColor = "green";
+                    new1 = document.getElementById("4" + id[1]);
+                    if ( new1.textContent == null|| new1.textContent.length == undefined || new1.textContent.length ==0) {
+                        new1.style.backgroundColor = "green";
+                    }
+                }
+            } else {
+                new1 = document.getElementById("3" + id[1])
+                if ( new1.textContent == null|| new1.textContent.length == undefined || new1.textContent.length ==0) {
+                    new1.style.backgroundColor = "green";
+                }
+            }
+        }
+        if ( text =="♖") {
+
+        }
+        if ( text =="♘") {
+
+        }
+        if ( text =="♗") {
+
+        }
+        if ( text =="♕") {
+            
+        }
+        if ( text =="♔") {
+
+        }
+
+
+        //Black
+        if (text == "♟") {
+
+        }
+        if (text == "♜") {
+            
+        }
+        if (text == "♞") {
+            
+        }
+        if (text == "♝") {
+            
+        }
+        if (text == "♛") {
+            
+        }
+        if (text == "♚") {
+            
+        }
+
+    }
+    
+});
+
+//Tries to highlight good moves ;(
